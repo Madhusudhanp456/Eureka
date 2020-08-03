da8d53c53934d709f6b5e67d31890c188567f2fb
madhusudhanp456 <madhusudhanp456@gmail.com>
2020-08-03 13:02:39 +0530

    MVC-JPA

---
+++ SpringMVCJpa/src/main/java/com/course/topic/TopicRepository.java
@@ -0,0 +1,17 @@
+package com.course.topic;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface TopicRepository extends CrudRepository<Topic,String>{
+
+	
+	
+
+	//getAllTopics()
+	//getTopic(String id)
+	//update topic(Topic t)
+	//delete topic(String id)
+	
+	
+	
+}
